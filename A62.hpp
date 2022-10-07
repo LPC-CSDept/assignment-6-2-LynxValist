bd1e09e85db8b08259c2a42b69e73b416d04d151
Gerald Kyro Villanueva Tungol <Lynxvillicana@gmail.com>
2022-10-06 19:21:26 -0700

    done

--- A62.hpp
+++ A62.hpp
@@ -7,10 +7,21 @@ using namespace std;
 // complete two functions
 double nthpowerp(int n)
 {
+        double pow = 1.0;
+        for(int i = 0; i < n; i++){
+                pow*=2;
+        }
+        return pow;
         // return n_th power number
 }
 double nthpowern(int n)
 {
+        double pow = 1.0;
+        for (int i = n; i < 0; i++){
+                pow*=2;
+        }
+        pow = 1.0/pow;
+        return pow;
         // return n_th power number
 }
 void printout(double result)
